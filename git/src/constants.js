a597fd5d32511e26ce85d6e261230cf000aa54bc
LucasZibell <lucas.zibell@wolox.com.ar>
2021-07-26 15:09:23 -0300

    change output of metrics

--- git/src/constants.js
+++ git/src/constants.js
@@ -1,13 +1,6 @@
 module.exports = {
-  gitMetrics: {
-    CODE_REVIEW_AVG_TIME: 'code-review-avg-time',
-    PICK_UP_TIME: 'pick-up-time'
-  },
   PROVIDERS: {
     github: 'github',
     gitlab: 'gitlab'
-  },
-  ERROR: {
-    REPO_NOT_FOUND: 'ERROR: Could not find repository'
   }
 };
--- git/src/providers/utils.js
+++ git/src/providers/utils.js
@@ -1,5 +1,12 @@
 // eslint-disable-next-line no-magic-numbers
 const MILLISECONDS_IN_HOURS = 1000 * 3600;
+const SEVEN_DAYS = 7;
 
 module.exports.getHoursBetween = (startDate, endDate) =>
   (new Date(endDate) - new Date(startDate)) / MILLISECONDS_IN_HOURS;
+
+module.exports.getSevenDaysAgo = () => {
+  const today = new Date();
+  today.setDate(today.getDate() - SEVEN_DAYS);
+  return today;
+};
