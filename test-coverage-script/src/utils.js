c9d82eefc2cfbc367491b30eb512dcf9c4e726db
Alejandro Bermudez <alejo0395@gmail.com>
2021-07-26 18:49:20 -0500

    removed folder

--- test-coverage-script/src/utils.js
+++
@@ -1,7 +0,0 @@
-const chalk = require('chalk');
-
-const error = (errorString) => console.error(chalk.red(errorString));
-const success = (successString) => console.log(chalk.green(successString));
-
-module.exports.error = error;
-module.exports.success = success;
