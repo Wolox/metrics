9f85b631863cefc19b214e7fef08cff47a976929
Alejandro Bermudez <alejo0395@gmail.com>
2021-08-06 17:04:34 -0500

    Refactor on script

--- js-test-coverage-script/src/utils.js
+++ js-test-coverage-script/src/utils.js
@@ -2,6 +2,13 @@ const chalk = require('chalk');
 
 const error = (errorString) => console.error(chalk.red(errorString));
 const success = (successString) => console.log(chalk.green(successString));
+const printError = (err) => {
+  error(err);
+  process.exitCode = 1;
+  process.exit();
+  return;
+};
 
 module.exports.error = error;
 module.exports.success = success;
+module.exports.printError = printError;
